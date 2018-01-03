5fca8392e158fe90781f3221bae0b8323a03c90c
Alice Xue <alicexue@Alices-MacBook-Pro.local>
2018-01-03 15:32:39 -0500

    added necessary info from MTurk, edited rerouting, optimized retrieval of auction data

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import jsonify
 import json
 import random
 import csv, os, sys
-import ast
-import socket
 from utils import * # importing * allows you to use methods from utils.py without calling utils.method_name
 from store_data import *
 from store_subject_info import *
@@ -24,9 +22,11 @@ GET: Passes list of dictionaries with stimulus information to auction.html
 POST: Saves auction data and stimuli to csv files, redirects to choice task
 
 """
-@app.route("/auction/<expID>", methods = ["GET","POST"])
-def auction(expID):
-	if 'demo' in request.args and 'workerID' in request.args:
+@app.route("/auction/<expId>", methods = ["GET","POST"])
+def auction(expId):
+	containsAllMTurkArgs = contains_all_args(request.args)
+
+	if 'demo' in request.args and containsAllMTurkArgs:
 		if request.method == "GET" and request.args.get('demo') == 'TRUE':
 			stimuli = get_stimuli('/static/stim/demo/')
 			random.shuffle(stimuli)
@@ -38,14 +38,23 @@ def auction(expID):
 
 			return render_template('auction.html', expVariables=expVariables, stimFolder='/static/stim/demo/')
 		else:
-			workerID = request.args.get('workerID')
-			return redirect(url_for('auction_instructions', expID = expID, workerID = workerID))
-	elif 'workerID' in request.args:
-		workerID = request.args.get('workerID')
 
-		completedAuction = completed_auction(expID, workerID)
+			workerId = request.args.get('workerId')
+			assignmentId = request.args.get('assignmentId')
+			hitId = request.args.get('hitId')
+			turkSubmitTo = request.args.get('turkSubmitTo')
 
-		if workerID_exists(expID, workerID) and completedAuction == False:
+			return redirect(url_for('auction_instructions', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
+	elif containsAllMTurkArgs:
+
+		workerId = request.args.get('workerId')
+		assignmentId = request.args.get('assignmentId')
+		hitId = request.args.get('hitId')
+		turkSubmitTo = request.args.get('turkSubmitTo')
+
+		completedAuction = completed_auction(expId, workerId)
+
+		if workerId_exists(expId, workerId) and completedAuction == False:
 			if request.method == "GET":
 				### set experiment conditions here and pass to experiment.html 
 				# trialVariables should be an array of dictionaries 
@@ -61,22 +70,22 @@ def auction(expID):
 
 				return render_template('auction.html', expVariables=expVariables, stimFolder='/static/stim/')
 			else:
-				subjectID = get_subjectID(expID, workerID)
+				subjectId = get_subjectId(expId, workerId)
 				expResults = json.loads(request.form['experimentResults'])
 
-				if not os.path.exists(expID):
-					os.makedirs(expID)
+				if not os.path.exists(expId):
+					os.makedirs(expId)
 
-				if not os.path.exists(expID + '/' + subjectID):
-					os.makedirs(expID + '/' + subjectID)
+				if not os.path.exists(expId + '/' + subjectId):
+					os.makedirs(expId + '/' + subjectId)
 
-				store_data(expID, expResults,'Auction',subjectID)
+				store_data(expId, expResults,'Auction',subjectId)
 
-				set_completed_auction(expID, workerID, True)
+				set_completed_auction(expId, workerId, True)
 
-				return redirect(url_for('choicetask_demo_instructions', expID=expID, workerID = workerID))
-		elif workerID_exists(expID, workerID) and completedAuction == True:
-			return redirect(url_for('auction_error', expID = expID, workerID = workerID))
+				return redirect(url_for('choicetask_demo_instructions', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
+		elif workerId_exists(expId, workerId) and completedAuction == True:
+			return redirect(url_for('auction_error', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
 		else:
 			return redirect(url_for('unauthorized_error'))
 	else:
@@ -90,9 +99,11 @@ GET: Retrieves stimulus ratings from auction data file, sets up stimuli for choi
 POST: Saves choice task data and stimuli to csv files, redirects to thank you page
 
 """
-@app.route("/choicetask/<expID>", methods = ["GET","POST"])
-def choicetask(expID):
-	if 'demo' in request.args and 'workerID' in request.args:
+@app.route("/choicetask/<expId>", methods = ["GET","POST"])
+def choicetask(expId):
+	containsAllMTurkArgs = contains_all_args(request.args)
+
+	if 'demo' in request.args and containsAllMTurkArgs:
 		if request.method == "GET" and request.args.get('demo') == 'TRUE':
 			[stim1Names, stim1Bids, stim2Names, stim2Bids] = get_two_stimuli_lists_without_bids('/static/stim/demo/')
 
@@ -104,54 +115,66 @@ def choicetask(expID):
 
 			for i in range(0,len(stim1Bids)):
 				expVariables.append({"stimulus1":stim1Names[i],"stimulus2":stim2Names[i],"stim1Bid":stim1Bids[i],"stim2Bid":stim2Bids[i], "delta":deltas[i]})
-			return render_template('choicetask.html', expID=expID, expVariables=expVariables, stimFolder='/static/stim/demo/')
+			return render_template('choicetask.html', expId=expId, expVariables=expVariables, stimFolder='/static/stim/demo/')
 		else:
-			workerID = request.args.get('workerID')
-			return redirect(url_for('choicetask_instructions', expID = expID, workerID = workerID))
-	elif 'workerID' in request.args:
-		workerID = request.args.get('workerID')
-		subjectID = get_subjectID(expID, workerID)
 
-		completedChoiceTask = completed_choice_task(expID, workerID)
+			workerId = request.args.get('workerId')
+			assignmentId = request.args.get('assignmentId')
+			hitId = request.args.get('hitId')
+			turkSubmitTo = request.args.get('turkSubmitTo')
 
-		if workerID_exists(expID, workerID) and completedChoiceTask == False:
+			return redirect(url_for('choicetask_instructions', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
+	elif containsAllMTurkArgs:
+
+		workerId = request.args.get('workerId')
+		subjectId = get_subjectId(expId, workerId)
+		assignmentId = request.args.get('assignmentId')
+		hitId = request.args.get('hitId')
+		turkSubmitTo = request.args.get('turkSubmitTo')
+
+		completedChoiceTask = completed_choice_task(expId, workerId)
+
+		if workerId_exists(expId, workerId) and completedChoiceTask == False:
 			if request.method == "GET":
 				### set experiment conditions here and pass to experiment.html 
 				# trialVariables should be an array of dictionaries 
 				# each element of the array represents the condition for one trial
 				# set the variable conditions to the array of conditions
 
-				stim1Bids = [];
-				stim2Bids = [];
+				if completed_auction(expId, workerId):
+					stim1Bids = [];
+					stim2Bids = [];
 
-				stimBidDict = get_bid_responses(_thisDir + '/' + expID + '/' + subjectID + '/' + subjectID + '_AuctionData.csv')
-				[stim1Names, stim1Bids, stim2Names, stim2Bids] = get_two_stimuli_lists(stimBidDict, '/static/stim/')
+					stimBidDict = get_bid_responses(_thisDir + '/' + expId + '/' + subjectId + '/' + subjectId + '_AuctionData.csv')
+					[stim1Names, stim1Bids, stim2Names, stim2Bids] = get_two_stimuli_lists(stimBidDict, '/static/stim/')
 
-				expVariables = [] # array of dictionaries
+					expVariables = [] # array of dictionaries
 
-				deltas = []
-				for i in range(0,len(stim1Bids)):
-					deltas.append(stim2Bids[i] - stim1Bids[i])
+					deltas = []
+					for i in range(0,len(stim1Bids)):
+						deltas.append(stim2Bids[i] - stim1Bids[i])
 
-				for i in range(0,len(stim1Bids)):
-					expVariables.append({"stimulus1":stim1Names[i],"stimulus2":stim2Names[i],"stim1Bid":stim1Bids[i],"stim2Bid":stim2Bids[i], "delta":deltas[i]})
+					for i in range(0,len(stim1Bids)):
+						expVariables.append({"stimulus1":stim1Names[i],"stimulus2":stim2Names[i],"stim1Bid":stim1Bids[i],"stim2Bid":stim2Bids[i], "delta":deltas[i]})
 
-				return render_template('choicetask.html', expID=expID, expVariables=expVariables, stimFolder='/static/stim/')
+					return render_template('choicetask.html', expId=expId, expVariables=expVariables, stimFolder='/static/stim/')
+				else:
+					return redirect(url_for('unauthorized_error'))
 			else:
 				expResults = json.loads(request.form['experimentResults'])
 
-				if not os.path.exists(expID):
-					os.makedirs(expID)
+				if not os.path.exists(expId):
+					os.makedirs(expId)
 
-				if not os.path.exists(expID + '/' + subjectID):
-					os.makedirs(expID + '/' + subjectID)
-				store_data(expID, expResults,'ChoiceTask', subjectID)
+				if not os.path.exists(expId + '/' + subjectId):
+					os.makedirs(expId + '/' + subjectId)
+				store_data(expId, expResults,'ChoiceTask', subjectId)
 
-				set_completed_choice_task(expID, workerID, True)
+				set_completed_choice_task(expId, workerId, True)
 
-				return redirect(url_for('thankyou'))
-		elif workerID_exists(expID, workerID) and completedChoiceTask == True:
-			return redirect(url_for('choicetask_error', expID = expID, workerID = workerID))
+				return redirect(url_for('thankyou', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
+		elif workerId_exists(expId, workerId) and completedChoiceTask == True:
+			return redirect(url_for('choicetask_error', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
 		else:
 			return redirect(url_for('unauthorized_error'))
 	else:
@@ -160,15 +183,23 @@ def choicetask(expID):
 """
 Auction Instructions
 """
-@app.route("/auction_instructions/<expID>", methods = ["GET","POST"])
-def auction_instructions(expID):
-	if 'workerID' in request.args:
-		workerID = request.args.get('workerID')
-		if workerID_exists(expID, workerID):
+@app.route("/auction_instructions/<expId>", methods = ["GET","POST"])
+def auction_instructions(expId):
+	if contains_all_args(request.args):
+
+		workerId = request.args.get('workerId')
+		assignmentId = request.args.get('assignmentId')
+		hitId = request.args.get('hitId')
+		turkSubmitTo = request.args.get('turkSubmitTo')
+
+		if workerId_exists(expId, workerId):
 			if request.method == "GET":
-				return render_template('auction_instructions.html', workerID = workerID)
+				return render_template('auction_instructions.html')
 			else:
-				return redirect(url_for('auction', expID = expID, workerID = workerID))
+				if request.form['submit'] == 'Continue':
+					return redirect(url_for('auction', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
+				else:
+					return redirect(url_for('auction_demo_instructions', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
 		else:
 			return redirect(url_for('unauthorized_error'))
 	else:
@@ -177,15 +208,22 @@ def auction_instructions(expID):
 """
 Auction Demo Instructions
 """
-@app.route("/auction_demo_instructions/<expID>", methods = ["GET","POST"])
-def auction_demo_instructions(expID):
-	if 'workerID' in request.args:
-		workerID = request.args.get('workerID')
-		if workerID_exists(expID, workerID):
+@app.route("/auction_demo_instructions/<expId>", methods = ["GET","POST"])
+def auction_demo_instructions(expId):
+	if 'assignmentId' in request.args and request.args.get('assignmentId') == 'ASSIGNMENT_ID_NOT_AVAILABLE':
+		redirect(url_for('accept_hit'))
+	elif contains_all_args(request.args) and request.args.get('assignmentId') != 'ASSIGNMENT_ID_NOT_AVAILABLE':
+
+		workerId = request.args.get('workerId')
+		assignmentId = request.args.get('assignmentId')
+		hitId = request.args.get('hitId')
+		turkSubmitTo = request.args.get('turkSubmitTo')
+
+		if workerId_exists(expId, workerId):
 			if request.method == "GET":
-				return render_template('auction_demo_instructions.html', workerID = workerID)
+				return render_template('auction_demo_instructions.html')
 			else:
-				return redirect(url_for('auction', expID = expID, workerID = workerID, demo = 'TRUE'))
+				return redirect(url_for('auction', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo, demo='TRUE'))
 		else:
 			return redirect(url_for('unauthorized_error'))
 	else:
@@ -194,15 +232,23 @@ def auction_demo_instructions(expID):
 """
 Choice Task Instructions
 """
-@app.route("/choicetask_instructions/<expID>", methods = ["GET","POST"])
-def choicetask_instructions(expID):
-	if 'workerID' in request.args:
-		workerID = request.args.get('workerID')
-		if workerID_exists(expID, workerID):
+@app.route("/choicetask_instructions/<expId>", methods = ["GET","POST"])
+def choicetask_instructions(expId):
+	if contains_all_args(request.args):
+
+		workerId = request.args.get('workerId')
+		assignmentId = request.args.get('assignmentId')
+		hitId = request.args.get('hitId')
+		turkSubmitTo = request.args.get('turkSubmitTo')
+
+		if workerId_exists(expId, workerId):
 			if request.method == "GET":
-				return render_template('choicetask_instructions.html', expID = expID, workerID = workerID)
+				return render_template('choicetask_instructions.html', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo)
 			else:
-				return redirect(url_for('choicetask', expID = expID, workerID = workerID))
+				if request.form['submit'] == 'Continue':
+					return redirect(url_for('choicetask', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
+				else:
+					return redirect(url_for('choicetask_demo_instructions', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo))
 		else:
 			return redirect(url_for('unauthorized_error'))
 	else:
@@ -211,15 +257,20 @@ def choicetask_instructions(expID):
 """
 Choice Task Demo Instructions
 """
-@app.route("/choicetask_demo_instructions/<expID>", methods = ["GET","POST"])
-def choicetask_demo_instructions(expID):
-	if 'workerID' in request.args:
-		workerID = request.args.get('workerID')
-		if workerID_exists(expID, workerID):
+@app.route("/choicetask_demo_instructions/<expId>", methods = ["GET","POST"])
+def choicetask_demo_instructions(expId):
+	if contains_all_args(request.args):
+
+		workerId = request.args.get('workerId')
+		assignmentId = request.args.get('assignmentId')
+		hitId = request.args.get('hitId')
+		turkSubmitTo = request.args.get('turkSubmitTo')
+
+		if workerId_exists(expId, workerId):
 			if request.method == "GET":
-				return render_template('choicetask_demo_instructions.html', expID = expID, workerID = workerID)
+				return render_template('choicetask_demo_instructions.html', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo)
 			else:
-				return redirect(url_for('choicetask', expID = expID, workerID = workerID, demo = 'TRUE'))
+				return redirect(url_for('choicetask', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo, demo='TRUE'))
 		else:
 			return redirect(url_for('unauthorized_error'))
 	else:
@@ -235,37 +286,53 @@ def MDMMT():
 		return render_template('consent_form.html')
 	else:
 		### need to change code here after testing is finished
-		workerID = 'abc' + str(random.randint(1000, 10000))
-		expID = 'MDMMT'
-		store_subject_info(expID, workerID)
-		return redirect(url_for('auction_demo_instructions', expID = expID, workerID = workerID, demo = 'TRUE'))
+		expId = 'MDMMT'
+		workerId = 'abc' + str(random.randint(1000, 10000))
+		assignmentId = 'xxxxx' + str(random.randint(10000, 100000))
+		hitId = 'hhhhh' + str(random.randint(10000, 100000))
+		turkSubmitTo = 'www.mturk.com'
+		store_subject_info(expId, workerId)
+		return redirect(url_for('auction_demo_instructions', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo, demo='TRUE'))
 
 @app.route("/thankyou", methods = ["GET"])
 def thankyou():
 	return render_template('thankyou.html')
 
-@app.route("/auction_error/<expID>", methods = ["GET", "POST"])
-def auction_error(expID):
-	if 'workerID' in request.args:
-		workerID = request.args.get('workerID')
+@app.route("/auction_error/<expId>", methods = ["GET", "POST"])
+def auction_error(expId):
+	if contains_all_args(request.args):
+		workerId = request.args.get('workerId')
+		assignmentId = request.args.get('assignmentId')
+		hitId = request.args.get('hitId')
+		turkSubmitTo = request.args.get('turkSubmitTo')
+
 		if request.method == "GET":
 			return render_template('auction_error.html')
 		else:
-			return redirect(url_for('choicetask', expID = expID, workerID = workerID, demo = 'TRUE'))
+			return redirect(url_for('choicetask_demo_instructions', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo, demo='TRUE'))
 	else:
 		return redirect(url_for('unauthorized_error'))
 
-@app.route("/choicetask_error/<expID>", methods = ["GET"])
-def choicetask_error(expID):
-	if 'workerID' in request.args:
-		workerID = request.args.get('workerID')
-		return render_template('choicetask_error.html')
+@app.route("/choicetask_error/<expId>", methods = ["GET"])
+def choicetask_error(expId):
+	if contains_all_args(request.args):
+		workerId = request.args.get('workerId')
+		assignmentId = request.args.get('assignmentId')
+		hitId = request.args.get('hitId')
+		turkSubmitTo = request.args.get('turkSubmitTo')
+
+		if request.method == "GET":
+			return redirect(url_for('thankyou', expId=expId, workerId=workerId, assignmentId=assignmentId, hitId=hitId, turkSubmitTo=turkSubmitTo, demo='TRUE'))
 	else:
 		return redirect(url_for('unauthorized_error'))
 
 @app.route("/unauthorized_error", methods = ["GET"])
 def unauthorized_error():
 	return render_template('unauthorized_error.html')
+
+@app.route("/accept_hit", methods = ["GET"])
+def accept_hit():
+	return render_template('accept_hit.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
--- store_subject_info.py
+++ store_subject_info.py
@@ -5,50 +5,50 @@ import datetime
 _thisDir = os.path.dirname(os.path.abspath(__file__)).decode(sys.getfilesystemencoding())
 os.chdir(_thisDir)
 
-def store_subject_info(expID, workerID):
-	if not os.path.exists(expID):
-		os.makedirs(expID)
-	csvLocation = _thisDir + '/' + expID +'/subject_worker_ids.csv'
+def store_subject_info(expId, workerId):
+	if not os.path.exists(expId):
+		os.makedirs(expId)
+	csvLocation = _thisDir + '/' + expId +'/subject_worker_ids.csv'
 	if not os.path.exists(csvLocation):
-		newSubjectID = expID + "%04d" % (1,)
+		newSubjectId = expId + "%04d" % (1,)
 		currentTime = datetime.datetime.now()
 		currentTime = currentTime.strftime("%Y-%m-%d %H:%M:%S")
-		newSubject = {'subjectID':newSubjectID, 'workerID':workerID, 'timeCreated':currentTime, 'completedAuction':False, 'completedChoiceTask':False}
+		newSubject = {'subjectId':newSubjectId, 'workerId':workerId, 'timeCreated':currentTime, 'completedAuction':False, 'completedChoiceTask':False}
 		new_df = pd.DataFrame(data=newSubject, index=[0])
 	else:
 		df = pd.read_csv(csvLocation)
 		nSubjects = len(df.index)
-		newSubjectID = expID + "%04d" % (nSubjects+1,)
+		newSubjectId = expId + "%04d" % (nSubjects+1,)
 		currentTime = datetime.datetime.now()
 		currentTime = currentTime.strftime("%Y-%m-%d %H:%M:%S")
-		newSubject = {'subjectID':newSubjectID, 'workerID':workerID, 'timeCreated':currentTime, 'completedAuction':False, 'completedChoiceTask':False}
+		newSubject = {'subjectId':newSubjectId, 'workerId':workerId, 'timeCreated':currentTime, 'completedAuction':False, 'completedChoiceTask':False}
 		df2 = pd.DataFrame(data=newSubject, index=[0])
 		new_df = pd.concat([df,df2], axis=0)
 	new_df.to_csv(csvLocation,index=False)
 
 # should assume subject did not participate before
-def get_subjectID(expID, workerID):
-	csvLocation = _thisDir + '/' + expID +'/subject_worker_ids.csv'
+def get_subjectId(expId, workerId):
+	csvLocation = _thisDir + '/' + expId +'/subject_worker_ids.csv'
 	df = pd.read_csv(csvLocation)
-	subjectIDs = df.loc[df['workerID'] == workerID]['subjectID'].values
-	if len(subjectIDs) > 0:
-		subjectID = df.loc[df['workerID'] == workerID]['subjectID'].values[0]
-		return subjectID
+	subjectIds = df.loc[df['workerId'] == workerId]['subjectId'].values
+	if len(subjectIds) > 0:
+		subjectId = df.loc[df['workerId'] == workerId]['subjectId'].values[0]
+		return subjectId
 	else: 
-		return False # workerID doesn't exist 
+		return False # workerId doesn't exist 
 
-def workerID_exists(expID, workerID):
-	subjectID = get_subjectID(expID, workerID)
-	if subjectID == False:
+def workerId_exists(expId, workerId):
+	subjectId = get_subjectId(expId, workerId)
+	if subjectId == False:
 		return False
 	else:
 		return True
 
-def completed_auction(expID, workerID):
-	csvLocation = _thisDir + '/' + expID +'/subject_worker_ids.csv'
+def completed_auction(expId, workerId):
+	csvLocation = _thisDir + '/' + expId +'/subject_worker_ids.csv'
 	df = pd.read_csv(csvLocation)
-	if workerID_exists(expID, workerID):
-		completed = df.loc[df['workerID'] == workerID]['completedAuction'].values[0]
+	if workerId_exists(expId, workerId):
+		completed = df.loc[df['workerId'] == workerId]['completedAuction'].values[0]
 		if completed == True:
 			return True
 		else:
@@ -56,11 +56,11 @@ def completed_auction(expID, workerID):
 	else:
 		return False
 
-def completed_choice_task(expID, workerID):
-	csvLocation = _thisDir + '/' + expID +'/subject_worker_ids.csv'
+def completed_choice_task(expId, workerId):
+	csvLocation = _thisDir + '/' + expId +'/subject_worker_ids.csv'
 	df = pd.read_csv(csvLocation)
-	if workerID_exists(expID, workerID):
-		completed = df.loc[df['workerID'] == workerID]['completedChoiceTask'].values[0]
+	if workerId_exists(expId, workerId):
+		completed = df.loc[df['workerId'] == workerId]['completedChoiceTask'].values[0]
 		if completed == True:
 			return True
 		else:
@@ -68,21 +68,21 @@ def completed_choice_task(expID, workerID):
 	else:
 		return False
 
-def set_completed_auction(expID, workerID, boole):
-	csvLocation = _thisDir + '/' + expID +'/subject_worker_ids.csv'
+def set_completed_auction(expId, workerId, boole):
+	csvLocation = _thisDir + '/' + expId +'/subject_worker_ids.csv'
 	df = pd.read_csv(csvLocation)
-	if workerID_exists(expID, workerID):
-		idx = df[df['workerID'] == workerID].index[0]
+	if workerId_exists(expId, workerId):
+		idx = df[df['workerId'] == workerId].index[0]
 		print df.loc[idx, 'completedAuction']
 		df.loc[idx, 'completedAuction'] = boole
 		print df.loc[idx, 'completedAuction']
 		df.to_csv(csvLocation,index=False)
 
-def set_completed_choice_task(expID, workerID, boole):
-	csvLocation = _thisDir + '/' + expID +'/subject_worker_ids.csv'
+def set_completed_choice_task(expId, workerId, boole):
+	csvLocation = _thisDir + '/' + expId +'/subject_worker_ids.csv'
 	df = pd.read_csv(csvLocation)
-	if workerID_exists(expID, workerID):
-		idx = df[df['workerID'] == workerID].index[0]
+	if workerId_exists(expId, workerId):
+		idx = df[df['workerId'] == workerId].index[0]
 		print df.loc[idx, 'completedChoiceTask']
 		df.loc[idx, 'completedChoiceTask'] = boole
 		print df.loc[idx, 'completedChoiceTask']
