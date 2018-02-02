9579614fa59e0038f1bd70639955ea7af1b800f7
Alice Xue <alice.m.xue@gmail.com>
2018-02-02 12:56:46 -0500

    changed rejection feedback

--- reject_assignment.py
+++ reject_assignment.py
@@ -15,7 +15,7 @@ if sys.argv[1] != "live" and sys.argv[1] != "sandbox":
 live = sys.argv[1] == "live"
 assignmentId = sys.argv[2]
 
-feedback = "Your responses do not meet our standards."
+feedback = """As we stated on the consent form, "Payment will be given upon verification that the survey was completed and response accuracy verified". In this study we asked you to rate food items and then choose between the food items you rated. Response accuracy is determined by the consistency of your choices in the 2nd task with your ratings of foods in the 1st task. To complete the HIT satisfactorily, you must have responded to at least 150 trials in the choice task and have been consistent at least 55% of the time. Your responses did not meet at least one criteria, so your HIT has been rejected."""
 args = ["aws", "mturk", "reject-assignment", "--assignment-id", assignmentId, "--requester-feedback", feedback]
 
 if not live:
